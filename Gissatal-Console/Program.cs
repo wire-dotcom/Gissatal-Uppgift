caddd90ca66bd047a5b4b5a5dc8f8c482b5389ae
wire-dotcom <wire03001@utb.vaxjo.se>
2020-11-23 09:27:45 +0100

    Commit 2: Fungerande spel som väljer ett slumpmässigt tal mellan 0 och 100, om spelaren gissar rätt så vinnar han/hon, och en liten bonus om spelaren gissar rätt på 10 eller mindre försök. Antal försök som krävdes räknas och skrivs ut efter spelaren vinner.

--- Gissatal-Console/Program.cs
+++ Gissatal-Console/Program.cs
@@ -13,7 +13,35 @@ namespace Gissatal_Console
             Random rndint = new Random(); // Våran slumpmäsiga int variabel.
             int rndtal = 0; // Definerar en int variabel som alltså kommer att vara talet som spelaren försöker gissa.
             rndtal = rndint.Next(0, 100); // Här sätts värdet på variablen till ett slumpmässigt tal mellan 0 och 100.
-            Console.WriteLine("hello wordl");
+            int antalGuess = 0;            
+            Console.WriteLine("Jag har valt ett tal, gissa vilket");
+
+            while (true)
+            {
+                string strGuess = Console.ReadLine();
+                int intGuess = Convert.ToInt32(strGuess);
+                Console.Clear();
+                antalGuess++;
+                if (intGuess == rndtal)
+                {
+                    Console.WriteLine("Du har gissat rätt!");
+                    if (antalGuess >= 10)
+                        Console.WriteLine("Det tog dig " + antalGuess + " försök");
+                    else
+                        Console.WriteLine("Det tog dig bara " + antalGuess + " försök");
+                    break;
+                }
+                else if (intGuess > rndtal)
+                {
+                    Console.WriteLine("Fel! Du har gissat för högt! Försök igen");
+
+                }
+                else
+                {
+                    Console.WriteLine("Fel! Du har gissat för lågt! Försök igen");
+                }    
+
+            }
         }
     }
 }
