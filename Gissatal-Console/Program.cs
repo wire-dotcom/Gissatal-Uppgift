58fa14dd7f40734f19037f2665215b9aa543d22e
wire-dotcom <wire03001@utb.vaxjo.se>
2020-11-23 08:59:43 +0100

    Add project files.

---
+++ Gissatal-Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gissatal_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndint = new Random(); // Våran slumpmäsiga int variabel.
+            int rndtal = 0; // Definerar en int variabel som alltså kommer att vara talet som spelaren försöker gissa.
+            rndtal = rndint.Next(0, 100); // Här sätts värdet på variablen till ett slumpmässigt tal mellan 0 och 100.
+        }
+    }
+}
